a079447f530597114a837718dfc642448c732f8c
Vitória Souza <vitoria.souza@acct.global>
2021-12-16 14:18:30 -0300

    added functionality

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { TimeSplit } from "./typings/global";
+import { tick, getTwoDaysFromNow } from "./utils/time";
 import { useCssHandles } from "vtex.css-handles";
 
 interface CountdownProps {
@@ -7,14 +9,24 @@ interface CountdownProps {
 
 const CSS_HANDLES = ["countdown"];
 
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
+
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate,
+  targetDate = DEFAULT_TARGET_DATE,
 }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
+
   const handles = useCssHandles(CSS_HANDLES);
+
+  tick(targetDate, setTime);
 
   return (
     <div className={`${handles.countdown} c-muted-1 db tc`}>
-      <h1>{targetDate}</h1>
+      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
     </div>
   );
 };
