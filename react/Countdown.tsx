7065b62eb8c70457035602042efe4678002447bf
Vitória Souza <vitoria.souza@acct.global>
2021-12-16 14:17:53 -0300

    added css handles

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useCssHandles } from "vtex.css-handles";
 
 interface CountdownProps {
   targetDate: string;
 }
 
+const CSS_HANDLES = ["countdown"];
+
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   targetDate,
 }) => {
+  const handles = useCssHandles(CSS_HANDLES);
+
   return (
-    <div>
+    <div className={`${handles.countdown} c-muted-1 db tc`}>
       <h1>{targetDate}</h1>
     </div>
   );
